94f6c72a7fb7492d36803cae8d85634c173b41e7
Umang Khare <umangkhare1407@gmail.com>
2021-07-21 12:00:06 +0530

    new commit

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
+#importing all the necessary libraries
 import pickle
 import re
 import numpy as np
 from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__)
+#loading the model
 model = pickle.load(open('irisModel.pkl', 'rb'))
 
+#app home page
 @app.route("/")
 def home():
     return render_template('index.html'), 'hello'
 
+#app prediction page
 @app.route("/predict", methods=['POST'])
 def predict():
+    #taking values of the features from form
     features = [int(x) for x in request.form.values()]
     features = [np.array(features)]
     prediction = model.predict(features)
